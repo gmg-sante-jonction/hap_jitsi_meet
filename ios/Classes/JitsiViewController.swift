3a525bbbbd65d6dc0cc5fe27304edb3762608cf0
gmg-sante-jonction <48997308+gmg-sante-jonction@users.noreply.github.com>
2022-05-17 15:59:55 +0530

    Update JitsiViewController.swift

--- ios/Classes/JitsiViewController.swift
+++ ios/Classes/JitsiViewController.swift
@@ -1,164 +1,169 @@
-import UIKit
-import JitsiMeetSDK
+@IBOutlet weak var videoButton: UIButton?
 
-class JitsiViewController: UIViewController {
+//CHEAT
+fileprivate var pipViewCoordinator: PiPViewCoordinator?
+fileprivate var jitsiMeetView: JitsiMeetView?
+
+var eventSink:FlutterEventSink? = nil
+var roomName:String? = nil
+var serverUrl:URL? = nil
+var subject:String? = nil
+var audioOnly:Bool? = false
+var audioMuted: Bool? = false
+var videoMuted: Bool? = false
+var token:String? = nil
+var featureFlags: Dictionary<String, Any>? = Dictionary();
+var zoomButton: UIButton?
+
+
+var jistiMeetUserInfo = JitsiMeetUserInfo()
+
+override func loadView() {
     
-    @IBOutlet weak var videoButton: UIButton?
+    super.loadView()
+}
+
+@objc func openButtonClicked(sender : UIButton){
     
-    fileprivate var pipViewCoordinator: PiPViewCoordinator?
-    fileprivate var jitsiMeetView: JitsiMeetView?
+    //openJitsiMeetWithOptions();
+}
+
+@objc func closeButtonClicked(sender : UIButton){
+    cleanUp();
+    self.dismiss(animated: true, completion: nil)
+}
+
+override func viewDidLoad() {
     
-    var eventSink:FlutterEventSink? = nil
-    var roomName:String? = nil
-    var serverUrl:URL? = nil
-    var subject:String? = nil
-    var audioOnly:Bool? = false
-    var audioMuted: Bool? = false
-    var videoMuted: Bool? = false
-    var token:String? = nil
-    var featureFlags: Dictionary<String, Any>? = Dictionary();
-    var zoomButton: UIButton?
+    //print("VIEW DID LOAD")
+    self.view.backgroundColor = .black
+    
+    super.viewDidLoad()
+}
+
+override func viewDidAppear(_ animated: Bool) {
+    openJitsiMeet();
+}
+
+override func viewWillTransition(to size: CGSize,
+                                 with coordinator: UIViewControllerTransitionCoordinator) {
+    super.viewWillTransition(to: size, with: coordinator)
+    let rect = CGRect(origin: CGPoint.zero, size: size)
+    pipViewCoordinator?.resetBounds(bounds: rect)
     
     
-    var jistiMeetUserInfo = JitsiMeetUserInfo()
+}
+
+func openJitsiMeet() {
+    cleanUp()
+    // create and configure jitsimeet view
+    let jitsiMeetView = JitsiMeetView()
     
-    override func loadView() {
+    
+    jitsiMeetView.delegate = self
+    self.jitsiMeetView = jitsiMeetView
+    let options = JitsiMeetConferenceOptions.fromBuilder { (builder) in
+        builder.welcomePageEnabled = true
+        builder.room = self.roomName
+        builder.serverURL = self.serverUrl
+        builder.subject = self.subject
+        builder.userInfo = self.jistiMeetUserInfo
+        builder.audioOnly = self.audioOnly ?? false
+        builder.audioMuted = self.audioMuted ?? false
+        builder.videoMuted = self.videoMuted ?? false
+        builder.token = self.token
         
-        super.loadView()
-    }
-    
-    @objc func openButtonClicked(sender : UIButton){
-        
-        //openJitsiMeetWithOptions();
-    }
-    
-    @objc func closeButtonClicked(sender : UIButton){
-        cleanUp();
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    override func viewDidLoad() {
-        
-        //print("VIEW DID LOAD")
-        self.view.backgroundColor = .black
-        super.viewDidLoad()
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        openJitsiMeet();
-    }
-    
-    override func viewWillTransition(to size: CGSize,
-                                     with coordinator: UIViewControllerTransitionCoordinator) {
-        super.viewWillTransition(to: size, with: coordinator)
-        let rect = CGRect(origin: CGPoint.zero, size: size)
-        pipViewCoordinator?.resetBounds(bounds: rect)
-    }
-    
-    func openJitsiMeet() {
-        cleanUp()
-        // create and configure jitsimeet view
-        let jitsiMeetView = JitsiMeetView()
-        
-        
-        jitsiMeetView.delegate = self
-        self.jitsiMeetView = jitsiMeetView
-        let options = JitsiMeetConferenceOptions.fromBuilder { (builder) in
-            builder.welcomePageEnabled = true
-            builder.room = self.roomName
-            builder.serverURL = self.serverUrl
-            builder.subject = self.subject
-            builder.userInfo = self.jistiMeetUserInfo
-            builder.audioOnly = self.audioOnly ?? false
-            builder.audioMuted = self.audioMuted ?? false
-            builder.videoMuted = self.videoMuted ?? false
-            builder.token = self.token
-            
-            self.featureFlags?.forEach{ key,value in
-                builder.setFeatureFlag(key, withValue: value);
-            }
-            
+        self.featureFlags?.forEach{ key,value in
+            builder.setFeatureFlag(key, withValue: value);
         }
         
-        jitsiMeetView.join(options)
-        
-        // Enable jitsimeet view to be a view that can be displayed
-        // on top of all the things, and let the coordinator to manage
-        // the view state and interactions
-        // pipViewCoordinator = PiPViewCoordinator(withView: jitsiMeetView)
-        // pipViewCoordinator?.configureAsStickyView(withParentView: view)
+    }
+    
+    jitsiMeetView.join(options)
+    
+    // Enable jitsimeet view to be a view that can be displayed
+    // on top of all the things, and let the coordinator to manage
+    // the view state and interactions
+    
+    //Original
+// pipViewCoordinator = PiPViewCoordinator(withView: jitsiMeetView)
+// pipViewCoordinator?.configureAsStickyView(withParentView: view)
 
-        pipViewCoordinator = PiPViewCoordinator(withView: jitsiMeetView)
-        pipViewCoordinator?.configureAsStickyView(withParentView: view.window)
-        
-        // animate in
-        jitsiMeetView.alpha = 0
-        pipViewCoordinator?.show()
-        // pipViewCoordinator?.enterPictureInPicture()
-        enterPicture(inPicture: [:])
-    }
+    //New code by Amol
+    pipViewCoordinator = PiPViewCoordinator(withView: jitsiMeetView)
+    pipViewCoordinator?.configureAsStickyView(withParentView: view.window)
     
-    func closeJitsiMeeting(){
-        jitsiMeetView?.leave()
-    }
-    
-    fileprivate func cleanUp() {
-        jitsiMeetView?.removeFromSuperview()
-        jitsiMeetView = nil
-        pipViewCoordinator = nil
-        //self.dismiss(animated: true, completion: nil)
-    }
+    // animate in
+    //jitsiMeetView.alpha = 0
+    pipViewCoordinator?.show()
+//as soon as start meet open in pip mode
+enterPicture(inPicture: [:])
+}
+
+func closeJitsiMeeting(){
+    jitsiMeetView?.leave()
+}
+
+fileprivate func cleanUp() {
+    jitsiMeetView?.removeFromSuperview()
+    jitsiMeetView = nil
+    pipViewCoordinator = nil
+    //self.dismiss(animated: true, completion: nil)
+}
 }
 
 extension JitsiViewController: JitsiMeetViewDelegate {
+
+func conferenceWillJoin(_ data: [AnyHashable : Any]!) {
+    //        print("CONFERENCE WILL JOIN")
+    var mutatedData = data
+    mutatedData?.updateValue("onConferenceWillJoin", forKey: "event")
+    self.eventSink?(mutatedData)
+}
+
+func conferenceJoined(_ data: [AnyHashable : Any]!) {
+    //        print("CONFERENCE JOINED")
+    var mutatedData = data
+    mutatedData?.updateValue("onConferenceJoined", forKey: "event")
+    self.eventSink?(mutatedData)
+}
+
+func conferenceTerminated(_ data: [AnyHashable : Any]!) {
+    //        print("CONFERENCE TERMINATED")
+    var mutatedData = data
+    mutatedData?.updateValue("onConferenceTerminated", forKey: "event")
+    self.eventSink?(mutatedData)
     
-    func conferenceWillJoin(_ data: [AnyHashable : Any]!) {
-        //        print("CONFERENCE WILL JOIN")
-        var mutatedData = data
-        mutatedData?.updateValue("onConferenceWillJoin", forKey: "event")
-        self.eventSink?(mutatedData)
-    }
-    
-    func conferenceJoined(_ data: [AnyHashable : Any]!) {
-        //        print("CONFERENCE JOINED")
-        var mutatedData = data
-        mutatedData?.updateValue("onConferenceJoined", forKey: "event")
-        self.eventSink?(mutatedData)
-    }
-    
-    func conferenceTerminated(_ data: [AnyHashable : Any]!) {
-        //        print("CONFERENCE TERMINATED")
-        var mutatedData = data
-        mutatedData?.updateValue("onConferenceTerminated", forKey: "event")
-        self.eventSink?(mutatedData)
-        
-        DispatchQueue.main.async {
-            self.pipViewCoordinator?.hide() { _ in
-                self.cleanUp()
-                self.dismiss(animated: true, completion: nil)
-            }
+    DispatchQueue.main.async {
+        self.pipViewCoordinator?.hide() { _ in
+            self.cleanUp()
+            self.dismiss(animated: true, completion: nil)
+            self.view.removeFromSuperview()
         }
-        
     }
     
-   func enterPicture(inPicture data: [AnyHashable : Any]!) {
-        //        print("CONFERENCE PIP IN")
-        self.view.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
-        var mutatedData = data
-        mutatedData?.updateValue("onPictureInPictureWillEnter", forKey: "event")
-        self.eventSink?(mutatedData)
-        DispatchQueue.main.async {
-            self.pipViewCoordinator?.enterPictureInPicture()
-        }
-        print("CONFERENCE PIP IN \(mutatedData)")
+}
+
+func enterPicture(inPicture data: [AnyHashable : Any]!) {
+    self.view.frame = CGRect(x: 0, y: 0, width: 1, height: 1)
+
+    var mutatedData = data
+    mutatedData?.updateValue("onPictureInPictureWillEnter", forKey: "event")
+    self.eventSink?(mutatedData)
+    DispatchQueue.main.async {
+        self.pipViewCoordinator?.enterPictureInPicture()
     }
     
-    func exitPictureInPicture() {
-        //        print("CONFERENCE PIP OUT")
-        var mutatedData : [AnyHashable : Any]
-        mutatedData = ["event":"onPictureInPictureTerminated"]
-        self.eventSink?(mutatedData)
-        print("CONFERENCE PIP OUT \(mutatedData)")
-    }
+            print("CONFERENCE PIP IN \(mutatedData)")
+
+}
+
+func exitPictureInPicture() {
+    var mutatedData : [AnyHashable : Any]
+    mutatedData = ["event":"onPictureInPictureTerminated"]
+    self.eventSink?(mutatedData)
+            print("CONFERENCE PIP OUT \(mutatedData)")
+
+}
 }
 
